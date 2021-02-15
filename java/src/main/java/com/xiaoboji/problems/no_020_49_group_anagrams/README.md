b7788316f31c68dc4080214ebb0c8b31b39f79d4
xiaoboji <xiaoboji1990@gmail.com>
2021-02-15 22:13:04 +0800

    update:group anagrams

--- java/src/main/java/com/xiaoboji/problems/no_020_49_group_anagrams/README.md
+++ java/src/main/java/com/xiaoboji/problems/no_020_49_group_anagrams/README.md
@@ -34,3 +34,34 @@ class Solution {
 }
 
 ```
+时间复杂度：时间复杂度：O(n(k+∣Σ∣))，其中n是strs中的字符串的数量，k是strs 中的字符串的的最大长度，Σ 是字符集，在本题中字符集为所有小写字母，|\Sigma|=26∣Σ∣=26。
+空间复杂度：时间复杂度：O(n(k+∣Σ∣))
+
+方法二：排序法
+```java
+class SolutionSort {
+  /**
+   * 由于互为字母异位词的两个字符串包含的字母相同，因此对两个字符串分别进行排序之后得到的字符串一定是相同的，故可以将排序之后的字符串作为哈希表的键。
+   * @param strs
+   * @return
+   */
+  public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+```
+
+时间复杂度：时间复杂度：O(nklogk)，其中n是strs中的字符串的数量，k是strs中的字符串的的最大长度。需要遍历n个字符串，对于每个字符串，需要 O(klogk)的时间进行排序以及 O(1)的时间更新哈希表，因此总时间复杂度是 O(nklogk)
+
+空间复杂度：空间复杂度：O(nk),需要用哈希表存储全部字符串。
+
---
+++ java/src/main/java/com/xiaoboji/problems/no_020_49_group_anagrams/[49]字母异位词分组_sort.java
@@ -0,0 +1,49 @@
+package com.xiaoboji.problems.no_020_49_group_anagrams;//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串 
+// 👍 654 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionSort {
+  /**
+   * 由于互为字母异位词的两个字符串包含的字母相同，因此对两个字符串分别进行排序之后得到的字符串一定是相同的，故可以将排序之后的字符串作为哈希表的键。
+   * @param strs
+   * @return
+   */
+  public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
