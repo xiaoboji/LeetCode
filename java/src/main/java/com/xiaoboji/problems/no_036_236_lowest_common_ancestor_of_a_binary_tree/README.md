fdd378c562d2ac5e412d71204773dd3d7945af9b
xiaoboji <xiaoboji1990@gmail.com>
2021-02-18 15:37:21 +0800

    add:236 lowest common ancestor of a binary tree

--- java/src/main/java/com/xiaoboji/problems/no_036_236_lowest_common_ancestor_of_a_binary_tree/README.md
+++ java/src/main/java/com/xiaoboji/problems/no_036_236_lowest_common_ancestor_of_a_binary_tree/README.md
@@ -1,6 +1,46 @@
 - leetcode链接
 
-[链接]()
+[236. 二叉树的最近公共祖先](https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/)
 - 解题思路
+    *  存储父节点方法,存储其中一个节点的所有父节点，然后逐个进行比较
+```
+class SolutionSave {
 
-- 其它
+    Map<Integer, TreeNode> parent = new HashMap<Integer, TreeNode>();
+    Set<Integer> visited = new HashSet<>();
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        // 先存储p的所有父节点
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
+    private void dfs(TreeNode root) {
+        // 存储当前节点和其父节点
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+}
+```
+复杂度分析
+
+时间复杂度：O(N)，其中N是二叉树的节点数。二叉树的所有节点有且只会被访问一次，从p和q节点往上跳经过的祖先节点个数不会超过N，因此总的时间复杂度为O(N)。
+
+空间复杂度：O(N)，其中N是二叉树的节点数。递归调用的栈深度取决于二叉树的高度，二叉树最坏情况下为一条链，此时高度为N，因此空间复杂度为O(N)，哈希表存储每个节点的父节点也需要 O(N) 的空间复杂度，因此最后总的空间复杂度为 O(N)。
+
---
+++ java/src/main/java/com/xiaoboji/problems/no_036_236_lowest_common_ancestor_of_a_binary_tree/[236]二叉树的最近公共祖先.java
@@ -0,0 +1,96 @@
+package com.xiaoboji.problems.no_036_236_lowest_common_ancestor_of_a_binary_tree;//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出：3
+//解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出：5
+//解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1,2], p = 1, q = 2
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [2, 105] 内。 
+// -109 <= Node.val <= 109 
+// 所有 Node.val 互不相同 。 
+// p != q 
+// p 和 q 均存在于给定的二叉树中。 
+// 
+// Related Topics 树 
+// 👍 938 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import com.xiaoboji.common.TreeNode;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class SolutionSave {
+
+    Map<Integer, TreeNode> parent = new HashMap<Integer, TreeNode>();
+    Set<Integer> visited = new HashSet<>();
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        // 先存储p的所有父节点
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
+    private void dfs(TreeNode root) {
+        // 存储当前节点和其父节点
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
